1a3ae2f4cc57489b1e2c7f1a372ae6c3d7e70515
maikatzchen <maikatzchen@gmail.com>
2025-07-18 16:33:17 +0800

    Update new.py

--- new.py
+++ new.py
@@ -380,11 +380,8 @@ else:
 
 col1, col2 = st.columns(2)
 # col3.metric("Current Rainfall (mm)", f"{rainfall_hour:.2f}")
-col1.metric(f"Rainfall (mm) [{source}]", f"{rainfall_day:.2f}")
-col2.metric(
-    "3-Day Rainfall (mm)",
-    f"{rainfall_3d:.2f}" if rainfall_3d is not None else "N/A"
-)
+col1.metric(f"Rainfall (mm)", f"{rainfall_day:.2f}")
+col2.metric("3-Day Rainfall (mm)", f"{rainfall_3d:.2f}" if rainfall_3d is not None else "N/A")
 
 
 # === Optional Map (showing location) ===
